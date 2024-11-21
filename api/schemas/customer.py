5c908c1b0cbe336bf544ea85a25e6c4d994cd6fd
moeezawann <moeez.awan15@gmail.com>
2024-11-21 14:43:50 -0500

    Adding customer.py files in relative folders and creating a test for customer

---
+++ api/schemas/customer.py
@@ -0,0 +1,30 @@
+from typing import Optional
+from pydantic import BaseModel
+from datetime import datetime
+
+class CustomerBase(BaseModel):
+    name: str
+    email: str
+    phone_number: str
+    address: str
+
+class CustomerCreate(CustomerBase):
+        password: str
+
+class CustomerUpdate(BaseModel):
+    name: str = None
+    email: Optional[str] = None 
+    phone_number: Optional[str] = None
+    address: Optional[str] = None
+    password: Optional[str] = None
+
+class CustomerLogin(BaseModel):
+        email: str
+        password: str
+
+class Customer(CustomerBase):
+    id: int
+    created_at: datetime
+
+    class Config:
+        orm_mode = True
