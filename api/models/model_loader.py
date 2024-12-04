aba50cba293efe443eeddfa8ebd4933cddf4d968
Haroon_Qadri <haroonqadri702@gmail.com>
2024-12-04 14:55:17 -0500

    fixed some issues in recipe and resources

--- api/models/model_loader.py
+++ api/models/model_loader.py
@@ -1,33 +1,14 @@
-
+from ..dependencies.database import Base, engine
 from . import (
-    orders,
-    order_details,
-    recipes,
-    sandwiches,
-    resources,
+    customer,
     menu_items,
     ingredients,
+    orders,
+    order_details,
     ratings_reviews,
-    customer,
-    transactions,
-    menu_item_ingredients,
     promotion,
 )
-from .junction_tables import menu_item_ingredients  # Correct file for the junction table
-from ..dependencies.database import engine
-
 
 def index():
-    orders.Base.metadata.create_all(engine)
-    order_details.Base.metadata.create_all(engine)
-    recipes.Base.metadata.create_all(engine)
-    sandwiches.Base.metadata.create_all(engine)
-    resources.Base.metadata.create_all(engine)
-    menu_items.Base.metadata.create_all(engine)
-    ingredients.Base.metadata.create_all(engine)
-    ratings_reviews.Base.metadata.create_all(engine)
-    customer.Base.metadata.create_all(engine)
-    menu_item_ingredients.create(engine, checkfirst=True)
-    transactions.Base.metadata.create_all(engine)
-    promotion.Base.metadata.create_all(engine)
-
+    # Create all tables at once
+    Base.metadata.create_all(engine)
